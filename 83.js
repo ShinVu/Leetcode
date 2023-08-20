c0fc047133eaf55d97bbf285f14a59834a077f0f
ShinnnVu <63784536+ShinnnVu@users.noreply.github.com>
2023-08-20 09:40:52 +0700

    Question 83

---
+++ 83.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  if (head == null) {
+    return null;
+  }
+  let ptr = new ListNode(head.val, null);
+  let result = ptr;
+  while (head != null) {
+    if (ptr.val !== head.val) {
+      ptr.next = head;
+      ptr = ptr.next;
+    }
+    head = head.next;
+  }
+  ptr.next = null;
+  return result;
+};
